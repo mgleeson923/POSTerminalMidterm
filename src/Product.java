516d17792566741e77bdbec9da77025e0ca72975
mgleeson923 <mgleeson923@gmail.com>
2017-07-12 14:44:18 -0400

    Generated Text File for Menu

--- src/Product.java
+++ src/Product.java
@@ -1,42 +1,48 @@
 import java.io.*;
-import java.util.Scanner;
 
 /**
  * Created by michaelgleeson on 7/12/17.
  */
 public class Product {
 
-    public static void readFromFile() {
+    private static void ReadFromFile() {
         try {
+            //read from a file
             File myFile = new File("menu.txt");
+
+            //FileReader is a connection stream for characters that connects to a text file
             FileReader reader = new FileReader(myFile);
+
+            //BufferReader allows the FileReader to chain to it
+            //it only goes back to check the file once the buffer is empty
             BufferedReader buff = new BufferedReader(reader);
-            String line;
+
+            //declare a String variable to hold each line as it's read from the file
+            String line = null;
+
+            // While loop (Read a line of text and assign it to "line"
+            //while that variable is NOT null, keep printing lines
             while ((line = buff.readLine()) != null) {
                 System.out.println(line);
             }
-            buff.close();
-        } catch (Exception e) {
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-
-    public static void writeToFile() {
-        // write to file
+    private static void WriteToFile() {
+        //write to file
         try {
-            Scanner scnr = new Scanner(System.in);
             FileWriter writer = new FileWriter("menu.txt", true);
-            writer.write("Cheeseburger" + "\n");
+            writer.write("Iced Tea Drink Sweetened Tea $0.99 \n");
             writer.close();
-//            if (!Validation.duplicateCountry(userCountry)) {
-//
-//            }else {
-//                System.out.println("Country already in list!");
-//            }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
+
