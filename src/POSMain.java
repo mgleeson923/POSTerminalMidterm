e0f1095be47977953a6fc7023314eae03720403f
elsharko1 <elsharko1@gmail.com>
2017-07-12 13:37:54 -0400

    change test

--- src/POSMain.java
+++ src/POSMain.java
@@ -3,6 +3,10 @@
  */
 public class POSMain {
     public static void main(String[] args) {
+
+
         System.out.println("Hello World");
+
+        System.out.println("Hola, como esta");
     }
 }
