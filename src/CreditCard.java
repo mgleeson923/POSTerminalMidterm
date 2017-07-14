5336539f6855f4cad3ef93011ed9d2daed229c4c
scott1vt <scott1vt@yahoo.com>
2017-07-14 14:08:55 -0400

    Payment Validator

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 /**
  * Midterm Project Grand Circus
- *
+ * <p>
  * Java June 2017
- *
+ * <p>
  * (Alphabetical Order)
  * Michael Gleeson
  * Matthew Menna
@@ -13,13 +13,14 @@ import java.util.Scanner;
  */
 public class CreditCard extends Payment {
 
+
     public String creditCardNum;
     public String cVV;
     public String expDate;
     public String paymentComplete;
 
     @Override
-    public String generatePayment(String a, String b, String c){
+    public String generatePayment(String a, String b, String c) {
 
         return paymentComplete;
     }
@@ -28,7 +29,14 @@ public class CreditCard extends Payment {
         System.out.println("Please enter your credit card number");
         creditCardNum = scan.nextLine();
 
+        while ((creditCardNum.length() >= 16) && (creditCardNum.length() <= 16)) {
+            System.out.println("Invalid credit card number! Please enter a  valid 16 credit card number again.");
+            //scan.nextLine();
+        }
+
         return creditCardNum;
+
+
     }
 
     public void setCreditCardNum(String creditCardNum) {
@@ -36,9 +44,13 @@ public class CreditCard extends Payment {
     }
 
     public String getcVV() {
-
         System.out.println("Please enter your CVV");
         cVV = scan.nextLine();
+
+        while ((cVV.length() < 3) && (cVV.length() > 3)) {
+            System.out.println("Invalid CVV! please enter a valid 3 digit CVV located on the back of your card. ");
+            //scan.nextLine();
+        }
         return cVV;
 
     }
@@ -50,6 +62,11 @@ public class CreditCard extends Payment {
     public String getExpDate() {
         System.out.println("Please enter your Exp Date");
         expDate = scan.nextLine();
+
+        while ((expDate.length() > 4) && (expDate.length() < 4)) {
+            System.out.println("Invalid Expiration Date! Please enter the exp date in format MM/YY");
+            scan.nextLine();
+        }
         return expDate;
     }
 
